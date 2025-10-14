d349e7b8080a82d60d90baeaa1db54855eab6a0d
Shruthika <shruthikapadma2004@gmail.com>
2025-10-15 01:56:41 +0530

    backend pushed

---
+++ src/main/java/com/klef/dom/JenkinsspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.klef.dom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsspringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/klef/dom/demoController.java
@@ -0,0 +1,5 @@
+package com.klef.dom;
+
+public class demoController {
+
+}
